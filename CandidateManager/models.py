45922d43a81f7a724d8bf7f4badd54ce7b220d8b
Patrick Feinson <PFeinsonDev@gmail.com>
2021-10-12 12:29:18 -0400

    feat:CandidateModel

--- CandidateManager/models.py
+++ CandidateManager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Candidate (models.Model):
+    # Relational Data
+    held_roles = models.__dict__ # Dictionary<HiringCompany, PreviousRole>
+    potential_roles = models.list # List<OpenRole>
+    # Skills Information
+    vetted_skills = models.__dict__ # Dictionary<Skill, SME>
+    unverfied_skills = models.list # List<Skill>
+    # Company Information
+    
