94a3f04ea721fde84af58c25cc8b73b515f0b49b
Patrick Feinson <Pfeinson@gmail.com>
2021-10-13 05:16:41 -0400

    feat: Basic Readme

--- CompanyManager/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CompanymanagerConfig(AppConfig):
-    name = 'CompanyManager'
--- RecruiterManager/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecruitermanagerConfig(AppConfig):
-    name = 'RecruiterManager'
