d11c31bed2c83011d74830327ba13150a6f49628
Patrick Feinson <PFeinsonDev@gmail.com>
2021-10-14 03:08:58 -0400

    feat: Create Role model Manager method

--- RoleManager/models.py
+++ RoleManager/models.py
@@ -1,6 +1,60 @@
 from django.db import models
 
-# Create your models here.
+class RoleManager(models.manager):
+    def create_role (request_data, self):
+        # Create model response dictionary
+        model_response = {}
+        error_list = []
+        model_response['status'] = True
+        # Validate Role Name
+        if request_data['role_name']:
+            if len(request_data['role_name']) < 1 : 
+                model_response['status'] = False
+                error_list.append("role name not long enough")
+            if len(request_data['role_name']) > 255 :
+                model_response['status'] = False
+                error_list.append('role name can not exceed 255 characters')
+        else:
+            model_response['status'] = False
+            error_list.append("role name not provided.")   
+        # Validate Role Description                    
+        if request_data['role_description']:
+            if len(request_data['role_description']) < 1 :
+                model_response['status'] = False
+                error_list.append("Role description must be at least 1 character lonng")
+            if len(request_data['role_description']) > 255 :
+                model_response['status'] = False
+                error_list.append("Role deescription can not exceeed 255 characters")         
+        else:
+            model_response['status'] = False
+            error_list.append("No role description provided.")
+        # Validate compensation amount
+        if request_data['compensation_amount']:
+            if request_data['compensation_amount'] < 0.01 :
+                model_response['status'] = False
+                error_list.append("Compensation cannot be less than 0.01")
+        else:
+            model_response['status'] = False
+            error_list.append("Compensation amount not provided")        
+        # Validate total_compensation_amount
+        if request_data['total_compensation_amount']:
+            if request_data['total_compensation_amount'] < request_data['compensation_amount'] :
+                model_response['status'] = False
+                error_list.append("Total compensation cannot be less than Base Compensation")
+        else:
+            model_response['status'] = False
+            error_list.append("Total Compensation Amount not provided")        
+        # Validate required_skills    
+        if model_response['status']:
+            # Create new Role
+            new_role = Role(
+                role_name = request_data['role_name'], 
+                role_description = request_data['role_description'],
+                compensation_amount = request_data['compensation_amount'],
+                total_compensation_amount = request_data['total_compensation_amount'],
+                required_skills = request_data['required_skills']
+            )
+            new_role.save()
 class Role(models.Model):
     role_name = models.CharField(max_length=255),
     role_description = models.CharField(max_length=255),
