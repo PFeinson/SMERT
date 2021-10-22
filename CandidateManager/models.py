38a7621e1cca8193f7a2a215e89fca6c79c4d9dc
ThermalVB <wyatt.horejsi@gmail.com>
2021-10-22 11:09:15 -0700

    Added Candidate validations and Create

--- CandidateManager/models.py
+++ CandidateManager/models.py
@@ -7,7 +7,70 @@ from ..PreviousRoleManager import PreviousRole
 from ..CompanyManager import HiringCompany
 from ..SkillsManager import Skill
 
-class Candidate (Professional):
+class candidate (Professional):
+    def create_candidate (request_data, self):
+        # Create model response dictionary
+        model_response = {}
+        error_list = []
+        model_response['status'] = True
+        # Validate candidate Name
+        if request_data['candidate_name']:
+            if request_data['candidate_name'].length < 1:
+                model_response['status'] = False
+                error_list.append("Name must be at least one character long")
+            else:
+                model_response['status'] = False
+                error_list.append("Name not provided")
+        # Validate candidate Birth Date
+        if request_data['candidate_birth_date']:
+            # This validation is going to require some timedate delta shit I'll write later
+            if request_data['candidate_birth_date'] < 18:
+                # Using 18 as a semi-arbitrary age, need to check with legal/HR
+                model_response['status'] = False
+                error_list.append("candidate must be at least 18 years old")
+        else:
+            model_response['status'] = False
+            error_list.append("Birth Date not provided")
+        # Validate candidate Contact Information
+        if request_data['candidate_address']:
+            # Need to hook in an address verification method
+            if not request_data['candidate_address'].verification:
+                model_response['status'] = False
+                error_list.append("Address cannot be verified")
+        else:
+            model_response['status'] = False
+            error_list.append("Address not provided")
+        if request_data['candidate_phone_number']:
+            # Need to hook in a phone number verification method
+            if not request_data['candidate_phone_number'].verification:
+                model_response['status'] = False
+                error_list.append("Phone number is not verified")
+        else:
+            model_response['status'] = False
+            error_list.append("Phone number not provided")
+        if request_data['candidate_email_address']:
+            # Need to hook in an email verification method
+            if not request_data['candidate_email_address'].verification:
+                model_response['status'] = False
+                error_list.append("Email is not verified")
+        else:
+            model_response['status'] = False
+            error_list.append("Email not provided")
+        
+        # Create new Candidate
+        if model_response['status']:
+            new_candidate = Professional(
+                candidate_name = request_data['candidate_name'],
+                candidate_birth_date = request_data['candidate_birth_date'],
+                candidate_address = request_data['candidate_address'],
+                candidate_phone_number = request_data['candidate_phone_number'],
+                candidate_email = request_data['candidate_email']
+            )
+            new_candidate.save()
+            model_response['new_candidate'] = new_candidate
+            model_response['status'] = True
+
+class candidate (Professional):
     # Previous Employment Information
     previousHiringCompaniesPlacedAt = models.list.ForeignKey(
         HiringCompany.company_name,
@@ -30,7 +93,7 @@ class Candidate (Professional):
     vetted_skills = models.list.ForeignKey(
         Skill,
         on_delete = models.CASCADE
-    ) # Dictionary<Skill, SME>
+    ) # Dictionary<Skill, candidate>
     unvetted_skills = models.list.ForeignKey(
         Skill,
         on_delete=models.CASCADE,
